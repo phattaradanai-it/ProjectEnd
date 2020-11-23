a0db358e1f89e5f4f74e62512391fde4bb282676
phattaradanai-it <b6070632@g.sut.ac.th>
2020-11-23 20:55:46 +0700

    Update index.php

--- Source_code/cert/index.php
+++ Source_code/cert/index.php
@@ -35,7 +35,7 @@ include "cert_function.php";
 <body>
     <?php include 'header.php';?>]
 
-    <h1>HELLO</h1>
+
     <!-- <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
